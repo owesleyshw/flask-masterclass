1d2e708e771c749bb57ea001f7640b81cd7d4db1
Wesley Júnior <agarwesley19@gmail.com>
2022-03-10 01:51:32 -0300

    updates

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -2,5 +2,5 @@ from app.model.post import Post
 
 class HomeController:
     def index(self, view, request):
-        posts = Post.query.all()
+        posts = Post.query.filter_by(published=True).all()
         return view('home/index.html', posts=posts)
